d7b106e6655bc28ecda0f782ca30ffcbfa94070e
David Sun <davidsun3000@hotmail.com>
2020-05-08 06:15:23 +0000

    delete weibo list

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -8,4 +8,12 @@
         </h5>
         {{ $status->content }}
     </div>
+
+    @can('destroy', $status)
+        <form action="{{ route('statuses.destroy', $status->id) }}" method="POST" onsubmit="return confirm('您确定要删除本条微博吗?')">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-smbtn-danger">删除</button>
+        </form>    
+    @endcan
 </li>
